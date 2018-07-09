79b1b00565a5a07b8a048b2a0640f819a8019170
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2018-07-09 19:29:19 -0300

    Layout, partials y las imagenes no van más al public

--- client/app.js
+++ client/app.js
@@ -24,6 +24,7 @@ app.use('/stylesheets', sass({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', searchRouter);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 //app.use('/items', itemRouter);
 
 app.set('view engine', 'pug');
