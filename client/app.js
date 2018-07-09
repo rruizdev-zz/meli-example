2d5737ef96690312063f70c66338a54f04068eab
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2018-07-09 05:27:01 -0300

    Bower, Pug, Sass y SearchBox

--- client/app.js
+++ client/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const sass = require('node-sass-middleware');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const searchRouter = require('./routes/search');
+//const itemRouter = require('./routes/item');
 
 const app = express();
 
@@ -12,9 +14,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use('/stylesheets', sass({
+    src: path.join(__dirname, 'scss'),
+    dest: path.join(__dirname, 'public/stylesheets'),
+    debug: true,
+    outputStyle: 'compressed'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', searchRouter);
+//app.use('/items', itemRouter);
+
+app.set('view engine', 'pug');
 
 module.exports = app;
