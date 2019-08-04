d92896a989a1d86e74b80fee448a7db8828359b2
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-08-04 16:41:50 -0300

    Organize components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+import { AppComponent } from './components/app.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
