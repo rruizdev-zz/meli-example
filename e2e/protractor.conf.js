9747885a2c1061b09ada88f7a33ff34a1a745b96
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-08-05 00:14:21 -0300

    Change port to 1810

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://localhost:1810/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
