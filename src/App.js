65e6d9f4425c916bc9a0571d411ff4c974c1c418
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-09-12 03:30:07 -0300

    Test with search page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Button } from 'react-bootstrap';
+import './App.sass';
+import logo from './Logo_ML.png';
+import search from './ic_Search.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="Search">
+      <Container>
+        <img className="Logo" src={logo} alt="" />
+        <form>
+          <input type="text" placeholder="Nunca dejes de buscar" required />
+          <Button><img src={search} alt="" /></Button>
+        </form>
+      </Container>
+    </header>
   );
 }
 
